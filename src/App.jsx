e56700bf91ffa64e089167bade4c5d0c7115fa60
Akshat1804 <86612849+Akshat1804@users.noreply.github.com>
2024-11-18 22:44:10 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline text-pink-400'>AKshat</h1>
+      <h1 className='text-3xl font-bold underline'>{import.meta.env.VITE_ADDGITHUB}</h1>
+    </>
+  )
+}
+
+export default App
+ 
